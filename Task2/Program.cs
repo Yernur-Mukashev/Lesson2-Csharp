2b48e3163471a68e1058b7b9d701cb1de1c07d0d
Ваше имя английскими буквами <>
2024-05-08 22:01:43 +0500

    add Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+Console.Clear();
+Console.Write("Введите A: ");
+int A = int.Parse(Console.ReadLine());
+Console.Write("Введите B: ");
+int B = int.Parse(Console.ReadLine());
+
+if (B * B == A)
+{
+  Console.WriteLine("A является квадратом B");
+}
+else
+{ 
+  Console.WriteLine("A не является квадратом В");
+}
